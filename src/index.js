a01969794d6c35eb15c5360b12fa0588565f09de
jason <jason@noisybox.net>
2026-05-31 21:53:15 -0700

    persist and tweaks

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@ function toggleDarkMode(){
   const currentTheme = document.documentElement.getAttribute('data-theme');
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
   document.documentElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', newTheme);
 }
 
 const searching = new Searching(rides, displayResults);
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: [{
             loader: 'url-loader',
             options: { 
-                limit: 8000, // Convert images < 8kb to base64 strings
+                limit: 5000, // Convert images < 5kb to base64 strings
                 name: 'images/[name].[ext]'
             } 
         }]
